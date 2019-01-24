df7aeb6a76ec0295af84038da5f92d617b008416
Dante <494941020@qq.com>
2019-01-24 17:35:50 +0800

    获取数据

---
+++ my-order/src/components/App/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import OrderList from '../OrderList';
+import './style.css';
+import Header from '../Header';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+      	<Header />
+      	<OrderList />
+      </div>
+    );
+  }
+}
+
+export default App;
